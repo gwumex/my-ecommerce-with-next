6bc35f24e71bbf5df84c8c880c74fd2048c12a8c
gwumex <42099116+gwumex@users.noreply.github.com>
2023-02-12 13:29:40 +0100

    checkout update

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -5,25 +5,29 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
     await connectDB();
     if(req.method !== 'POST') {
-        res.send('ok')
+        res.json('Not a post request')
     }
+    const {email} = req.body
     const productsIds = req.body.products.split(',');
     const uniqIds = [...new Set(productsIds)];
     const products = await Product.find({_id:{$in:uniqIds}}).exec()
 
+    let line_items = []
     for (let productId of uniqIds){
-        const quantity = productsIds.find(id => productId).length;
-        
+        const quantity = productsIds.filter(id => id === productId).length;
+        const product = products.find(p => p._id.toString() === productId);
+        line_items.push({
+          quantity,
+          price_data: {
+            currency: 'USD',
+            product_data: {name: product.name},
+            unit_amount: product.price * 100
+          },
+         })
     }
 
     const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
+        line_items: line_items,
         mode: 'payment',
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
